fd461b0119d45dfcfc298df07aeff8a1e7ab1589
Dmitry Shemin <shemindmitry@gmail.com>
2023-10-28 09:48:38 +0700

    feat(api): add full migration

--- migrations/20230804154753_initialize.sql
+++ migrations/20230804154753_initialize.sql
@@ -4,7 +4,38 @@ CREATE TABLE "storages" (
     "settings" JSONB NOT NULL
 );
 
-CREATE TABLE "users" (
+CREATE TABLE "books" (
     "id" UUID PRIMARY KEY,
-    "role" VARCHAR(50) NOT NULL
+    "storage_id" UUID NOT NULL REFERENCES "storages"("id") ON DELETE RESTRICT,
+    "title" VARCHAR(1024) NOT NULL,
+    "path" JSONB NOT NULL
+);
+
+CREATE TABLE "tags" (
+    "id" UUID PRIMARY KEY,
+    "name" VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE "books_tags" (
+    "book_id" UUID NOT NULL REFERENCES "books"("id") ON DELETE CASCADE,
+    "tag_id" UUID NOT NULL REFERENCES "tags"("id") ON DELETE CASCADE,
+    PRIMARY KEY ("book_id", "tag_id")
+);
+
+CREATE TABLE "highlights" (
+    "id" UUID NOT NULL,
+    "book_id" UUID NOT NULL REFERENCES "books"("id") ON DELETE CASCADE,
+    "page" SMALLINT NOT NULL,
+    "start" SMALLINT NOT NULL,
+    "end" SMALLINT NOT NULL,
+    "title" VARCHAR(512) NOT NULL,
+    "note" TEXT
+);
+
+CREATE TABLE "bookmarks" (
+    "id" UUID NOT NULL,
+    "book_id" UUID NOT NULL REFERENCES "books"("id") ON DELETE CASCADE,
+    "page" SMALLINT NOT NULL,
+    "title" VARCHAR(512) NOT NULL,
+    "note" TEXT
 );
