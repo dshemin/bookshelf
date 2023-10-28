7ea2c63b8895ee7430a4aa419b0a02f163175481
Dmitry Shemin <shemindmitry@gmail.com>
2023-10-28 09:54:04 +0700

    fix: linter issue

--- migrations/20230804154753_initialize.sql
+++ migrations/20230804154753_initialize.sql
@@ -6,7 +6,7 @@ CREATE TABLE "storages" (
 
 CREATE TABLE "books" (
     "id" UUID PRIMARY KEY,
-    "storage_id" UUID NOT NULL REFERENCES "storages"("id") ON DELETE RESTRICT,
+    "storage_id" UUID NOT NULL REFERENCES "storages" ("id") ON DELETE RESTRICT,
     "title" VARCHAR(1024) NOT NULL,
     "path" JSONB NOT NULL
 );
@@ -17,14 +17,14 @@ CREATE TABLE "tags" (
 );
 
 CREATE TABLE "books_tags" (
-    "book_id" UUID NOT NULL REFERENCES "books"("id") ON DELETE CASCADE,
-    "tag_id" UUID NOT NULL REFERENCES "tags"("id") ON DELETE CASCADE,
+    "book_id" UUID NOT NULL REFERENCES "books" ("id") ON DELETE CASCADE,
+    "tag_id" UUID NOT NULL REFERENCES "tags" ("id") ON DELETE CASCADE,
     PRIMARY KEY ("book_id", "tag_id")
 );
 
 CREATE TABLE "highlights" (
     "id" UUID NOT NULL,
-    "book_id" UUID NOT NULL REFERENCES "books"("id") ON DELETE CASCADE,
+    "book_id" UUID NOT NULL REFERENCES "books" ("id") ON DELETE CASCADE,
     "page" SMALLINT NOT NULL,
     "start" SMALLINT NOT NULL,
     "end" SMALLINT NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE "highlights" (
 
 CREATE TABLE "bookmarks" (
     "id" UUID NOT NULL,
-    "book_id" UUID NOT NULL REFERENCES "books"("id") ON DELETE CASCADE,
+    "book_id" UUID NOT NULL REFERENCES "books" ("id") ON DELETE CASCADE,
     "page" SMALLINT NOT NULL,
     "title" VARCHAR(512) NOT NULL,
     "note" TEXT
