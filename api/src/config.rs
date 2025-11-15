2cf7081ccee8e91f43bb10ea796bdf27dcf5f4b5
Dmitry Shemin <shemindmitry@gmail.com>
2025-11-16 00:31:17 +0700

    chore: remove unnecessary lifetime param

--- api/src/config.rs
+++ api/src/config.rs
@@ -38,7 +38,7 @@ impl fmt::Display for Secret {
 }
 
 impl fmt::Debug for Secret {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "*******")
     }
 }
