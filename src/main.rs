3a5a15f761f77566830fabd2fd15dfea7246df3f
Dmitry Shemin <shemindmitry@gmail.com>
2026-02-23 21:45:27 +0700

    chore: add gracefull stop

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod config;
 
 use axum::{Router, routing::get};
 use log::info;
-use tokio::sync::Mutex;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -19,9 +19,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/readiness", get(|| async { "Ok" }))
         .with_state(state);
 
-    info!(config:? = &cfg; "starting server");
+    info!(config:? = &cfg; "start server");
     let listener = tokio::net::TcpListener::bind(cfg.address).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
     Ok(())
 }
 
@@ -32,6 +34,24 @@ fn setup_logger() {
         .format_file(true)
         .format_line_number(true)
         .init();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c().await.expect("can't install CTRL-C signal");
+    };
+
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => { info!("stop server") },
+        _ = terminate => { info!("stop server") },
+    }
 }
 
 #[derive(Clone)]
