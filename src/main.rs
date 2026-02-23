753143c60af57d986bb986b8a7202ce0cee31c43
Dmitry Shemin <shemindmitry@gmail.com>
2026-02-23 21:32:26 +0700

    api: setup logger

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    setup_logger();
 
     let cfg = config::load()?;
 
@@ -19,10 +19,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/readiness", get(|| async { "Ok" }))
         .with_state(state);
 
-    info!(config:? = &cfg; "starting server...");
+    info!(config:? = &cfg; "starting server");
     let listener = tokio::net::TcpListener::bind(cfg.address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
     Ok(())
+}
+
+fn setup_logger() {
+    env_logger::builder()
+        .parse_env("BOOKSHELF_LOG")
+        .filter_level(log::LevelFilter::Info)
+        .format_file(true)
+        .format_line_number(true)
+        .init();
 }
 
 #[derive(Clone)]
